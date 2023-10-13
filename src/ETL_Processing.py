b8aad477c6af314b19739d1174955306da7ca61f
Bernardo Lima <bernardo.lima.93@hotmail.com>
2023-10-13 14:25:16 -0300

    :sparkles: ci: criacao executavel do tratamento de dados

---
+++ src/ETL_Processing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import datetime
+
+df = pd.read_csv('/home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/base_1.csv', sep = ';',encoding='iso-8859-1')
+df['DATA/HORA'] = pd.to_datetime(df['DATA/HORA'],dayfirst=True)
+df = df[['DATA/HORA','ID_ALARME','ESTADO','SEVERIDADE','CÓDIGO','DESCRIÇÃO','LOCALIZAÇÃO']]
+df['ID_ALARME'] = df['ID_ALARME'].astype(str)
+df['ESTADO'] = df['ESTADO'].astype(bool)
+df.to_csv("/home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/base_ETL_True.csv",index=False)
+
+print("Sucessful Data Processing!")
---
+++ src/Processing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/venv-tcc/bin/activate
+
+python /home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/ETL_Processing.py
