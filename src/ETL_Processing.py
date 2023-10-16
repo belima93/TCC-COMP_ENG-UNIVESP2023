8e97c378e2e3522823490d3850f5f2a6712bfc00
Bernardo Lima <bernardo.lima.93@hotmail.com>
2023-10-16 13:57:49 -0300

    :boom: corrigido a executacao do script

--- src/ETL_Processing.py
+++ src/ETL_Processing.py
@@ -9,3 +9,11 @@ df['ESTADO'] = df['ESTADO'].astype(bool)
 df.to_csv("/home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/base_ETL_True.csv",index=False)
 
 print("Sucessful Data Processing!")
+
+df_falhas = df[df['ESTADO']==True]
+df_falhas = df_falhas[['CÓDIGO','DESCRIÇÃO']]
+df_falhas = df_falhas.drop_duplicates()
+df_falhas = df_falhas.dropna()
+df_falhas.to_csv("/home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/base_code_failure.csv",index=False)
+
+print(" Sucess in updating the Failure Table !")
