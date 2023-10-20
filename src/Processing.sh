2ba4a1c1155dbea0f6ed1f6fb55fdc6cb8367fa1
Bernardo Lima <bernardo.lima.93@hotmail.com>
2023-10-19 23:04:00 -0300

    :bricks: ci: Atualizado o script de exec ETL

--- src/Processing.sh
+++ src/Processing.sh
@@ -3,3 +3,5 @@
 source /home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/venv-tcc/bin/activate
 
 python /home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/ETL_Processing.py
+
+python /home/b3r/Desktop/TCC-COMP_ENG-UNIVESP2023/src/ETL_Dict_Local.py
